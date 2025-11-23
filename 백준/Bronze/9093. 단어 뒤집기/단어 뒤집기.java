eba81ffe965458cb9d71222cd566e9bdf6499381
kkisim <kisimoko@gmail.com>
2025-11-23 17:43:00 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 400 ms, Memory: 54724 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,51 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        C c = new C();
+        c.setting();
+        c.print();
+    }
+}
+class C {
+    BufferedReader br;
+    StringBuilder sb;
+
+    void setting() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine()); 
+
+        for (int t = 0; t < T; t++) {
+            String str = br.readLine(); 
+            String[] w = str.split(" "); 
+
+            StringBuilder st = new StringBuilder();
+
+            for (int i = 0; i < w.length; i++) {
+                String word = w[i];
+
+                String reversed = new StringBuilder(word).reverse().toString();
+
+                st.append(reversed);
+
+                if (i != w.length - 1) {
+                    st.append(' ');
+                }
+            }
+
+            sb.append(st).append('\n');
+        }
+    }
+
+    void print() {
+        System.out.print(sb);
+    }
+}
+
+
